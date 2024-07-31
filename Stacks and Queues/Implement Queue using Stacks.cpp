7e0901b8fb67ced56621fac346fbb12e4103875e
akarshmadan4 <100418866+akarshmadan4@users.noreply.github.com>
2024-07-31 20:18:36 +0530

    Day15

---
+++ Stacks and Queues/Implement Queue using Stacks.cpp
@@ -0,0 +1,33 @@
+class MyQueue {
+public:
+    stack<int> input,output;
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        input.push(x);
+        
+    }
+    
+    int pop() {
+        int top=peek();output.pop();
+        return top;
+    }
+    
+    int peek() {
+        if(output.empty())
+        {
+            while(input.size())
+            {
+                output.push(input.top());input.pop();
+            }
+        }
+        return output.top();
+    }
+    
+    bool empty() {
+        return (input.empty() && output.empty());
+        
+    }
+};
