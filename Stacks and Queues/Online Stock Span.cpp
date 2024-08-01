3d4bc95022906bb54b12b889310754d848e50c4d
akarshmadan4 <100418866+akarshmadan4@users.noreply.github.com>
2024-08-01 21:06:31 +0530

    Day16

---
+++ Stacks and Queues/Online Stock Span.cpp
@@ -0,0 +1,18 @@
+class StockSpanner {
+public:
+    StockSpanner() {
+        
+    }
+    stack<pair<int,int>>s;
+    int next(int price) {
+        int res=1;
+        while(!s.empty() && s.top().first<=price)
+        {
+            res+=s.top().second;
+            s.pop();
+        }
+        s.push({price,res});
+        return res;
+        
+    }
+};
